90d6e37a9345f4b4048e99761425f48b59d68292
yasirsaleemvws <126661150+Yasir365@users.noreply.github.com>
2024-08-23 18:47:29 +0500

    update 0.8

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+$(document).ready(function () {
+    $('.nav-link').click(function (event) {
+        if (this.hash !== "" && this.hash !== "#") {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 50);
+        }
+    });
+
+    $('#link1').click(function () {
+        $('.nav-item').removeClass('active');
+        $('#home').addClass('active');
+    });
+    $('#link2').click(function () {
+        $('.nav-item').removeClass('active');
+        $('#home').addClass('active');
+    });
+});
+
 $('.owl-carousel').owlCarousel({
     stagePadding: 50,
     loop: true,
